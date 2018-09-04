09fa559dbaf13ddf9062aefb561fb4ab1100a776
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-09-04 09:53:28 +0300

    Setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import pkg from './package.json'
+import glsl from 'rollup-plugin-glsl'
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.umd, format: 'umd', name: 'Canvas' },
+    { file: pkg.umd, format: 'umd', name: 'Webgl' },
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'es' }
+  ],
+  plugins: [
+  	glsl({
+  		include: '**/*.glsl'
+  	})
   ]
 }
